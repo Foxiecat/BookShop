f6e5aca909aea53da280c85bdb31efb3a45b34b7
Danica Ellefsen Kvilhaug <65165280+Foxiecat@users.noreply.github.com>
2024-09-26 13:00:54 +0200

    Init

---
+++ BookShop/Features/Books/Book.cs
@@ -0,0 +1,11 @@
+namespace BookShop.Features.Books;
+
+public class Book
+{
+    public int Id { get; init; }
+    public string Title { get; init; }
+    public string Author { get; init; }
+    public int PublicationYear { get; init; }
+    public string ISBN { get; init; }
+    public int InStock { get; init; }
+}
